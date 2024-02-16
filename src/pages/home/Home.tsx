4d671f143aaca2b7b3cb9f2bb08dbcdd21bd70a3
carina-maleski <carinamaleski@gmail.com>
2024-02-16 16:35:49 -0300

    config

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <h3 className="text-xl">justify-content alinhamento horizontal</h3>
+      </div>
+
+      <div>
+        <h3 className="text-xl">justify-content alinhamento vertical</h3>
+      </div>
+    </>
+  );
+}
+
+export default Home;
